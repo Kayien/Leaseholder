34d63268377b5c2997367188489be39927e7f162
Juan Carlos Arrieta Rincón <juan.arrieta.r@gmail.com>
2021-03-10 09:15:07 -0400

    DataModel Done,

---
+++ ASPNET_Core_2_1/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using ASPNET_Core_2_1.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNET_Core_2_1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Contract> Contracts { get; set; }
+
+        public DbSet<Lessee> Lessees { get; set; }
+
+        public DbSet<Owner> Owners { get; set; }
+
+        public DbSet<Property> Properties { get; set; }
+
+        public DbSet<PropertyImage> PropertyImages { get; set; }
+
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+    }
+
+}
